a14fe98dfe07d0fc0c3c7b621e1e09c7efa53c14
dam <dam@203PC09.intern>
2018-02-21 13:15:13 +0100

    Primer Commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio de Entornos de GITHUB.
+ */
+package holamundo;
+
+/**
+ *
+ * @author PilarMonica
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("HOLA MUNDO");
+    }
+    
+}
